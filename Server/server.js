243d0fa8c44b734626a915ece600286c021cb23d
Aastha Shahi <aasthashahi561@gmail.com>
2026-08-16 22:05:19 +0545

    Update server configuration

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // <-- add this to handle form-data
+app.use(express.urlencoded({ extended: true })); 
 app.use(cors());
 
 
@@ -20,10 +21,13 @@ const borrowRoutes = require("./routes/borrowRoutes");
 const userRoutes = require("./routes/userRoutes");
 const bookRoutes = require("./routes/bookRoutes");
 
+
 app.use("/api/auth", authRoutes);
 app.use("/api/borrow", borrowRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/books", bookRoutes);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 
 
