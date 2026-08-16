5129ea7429c10579b33c02d9a1aa7cc8e192a86b
Aastha Shahi <aasthashahi561@gmail.com>
2026-08-16 22:04:56 +0545

    Update book routes

--- Server/routes/bookRoutes.js
+++ Server/routes/bookRoutes.js
@@ -7,12 +7,13 @@ const { addBook,
     deleteBook} = require("../controllers/bookController");
 const {authMiddleware} = require("../middleware/authMiddleware");
 const checkRole = require("../middleware/roleMiddleware");
+const { upload } = require("../middleware/upload");
 
 router.get("/", getBooks);
 router.get("/:id", getBookById);
 
-router.post("/",authMiddleware, checkRole("librarian"), addBook);
+router.post("/",authMiddleware, checkRole("librarian"), upload.single("image"),addBook);
 
-router.put("/:id",authMiddleware, checkRole("librarian"), updateBook);
+router.put("/:id",authMiddleware, checkRole("librarian"), upload.single("image"), updateBook);
 router.delete("/:id",authMiddleware, checkRole("librarian"), deleteBook);
 module.exports = router;
