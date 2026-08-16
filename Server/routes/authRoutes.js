8f56c251c98d8da02782759a689d855cad132c1d
Aastha Shahi <aasthashahi561@gmail.com>
2026-08-16 22:04:48 +0545

    Add authentication routes

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, registerLibrarian } = require("../controllers/authControllers");
+const { register, login, registerLibrarian, getMe, updateMe } = require("../controllers/authControllers");
 const { authMiddleware } = require("../middleware/authMiddleware");
 const checkRole = require("../middleware/roleMiddleware");
+const { uploadAvatar } = require("../middleware/upload");
 
-
-router.post("/register", register);  
+router.post("/register", register);
 router.post("/login", login);
 
-
 router.post("/register-librarian", authMiddleware, checkRole("admin"), registerLibrarian);
+
+// Own profile
+router.get("/me", authMiddleware, getMe);
+router.put("/me", authMiddleware, uploadAvatar.single("avatar"), updateMe);
 
 module.exports = router;
