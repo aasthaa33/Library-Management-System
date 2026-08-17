ebae156d0fb34af117bfa04b2e48a66dd2cdb750
Aastha Shahi <aasthashahi561@gmail.com>
2026-08-17 13:04:41 +0545

    Update about page

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 export default function About() {
   const navigate = useNavigate();
 
-  const userRole = localStorage.getItem("role"); 
+  const userRole = sessionStorage.getItem("role"); 
 
   const handleLearnMore = () => {
     if (!userRole) {
